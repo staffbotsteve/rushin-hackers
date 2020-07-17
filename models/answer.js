ecea44afaf68550c35b831f39fe64e9050348be3
Mark Carlson <mcarlson@avenuecode.com>
2020-07-16 22:21:00 -0700

    Fixes answer.js

--- models/answer.js
+++ models/answer.js
@@ -14,6 +14,6 @@ module.exports = function(sequelize, DataTypes) {
     Answer.belongsToMany(models.Choice, {
       through: "AnswerChoices"
     });
-    return Answer;
   };
+  return Answer;
 };
